0d38c0f9c1bf85874be5f6acec1259273e96caf8
yechoi42 <yechoi@student.42seoul.kr>
2020-09-03 15:13:59 +0900

    main

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char *g_line; 
+
+typedef struct s_env 
+{
+	char *key;
+	char *value;
+} t_env;
+
+t_list	*get_envs(int argc, char **argv, char **envp)
+{
+	t_list	*envs;
+	t_env	*env;
+	int 	pos;
+	
+	if (!argc && argv)
+		exit(1);
+	while (*envp)
+	{
+		env = (t_env *)malloc(sizeof(t_env));
+		pos = ft_strchr(*envp, '=') - *envp;
+		env->key = ft_substr(*envp, 0, pos - 1);
+		env->value = ft_substr(*envp, pos + 1, ft_strlen(*envp) - pos - 1);
+		ft_lstadd_back(&envs, ft_lstnew(env));
+		envp++;
+	}
+	return (envs);
+}
+
+void	catch_sigint()
+{
+	ft_putstr_fd("\n◕_◕ ༽つ", 1);
+}
+
+void	catch_signals(void)
+{
+	signal(SIGINT, catch_sigint);
+	//should add SIGQUIT func
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	t_list *envs;
+	g_line = 0;
+	envs = get_envs(argc, argv, envp);
+	catch_signals();
+	while (1)
+	{
+		ft_putstr_fd("◕_◕ ༽つ", 1);
+		get_next_line(0, &g_line);
+	}
+	return (0);
+}
+

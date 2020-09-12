0ae9dc061b0fe7b9a20928fc04d1b9d013edd1ce
jwon42 <jwon@student.42seoul.kr>
2020-09-12 14:22:57 +0900

    FIX: signal.c

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,6 +6,7 @@ void 	handle_signal(int signo)
 
 	if (signo == SIGINT)
 	{
+		ft_putstr_fd("\b\b  \b\b", 1);
 		ft_putstr_fd("\n", 1);
 		show_prompt(g_env_user);
 	}
@@ -13,8 +14,9 @@ void 	handle_signal(int signo)
 	{
 		signo = wait(&status);
 		g_exit_value = status / 256;
+		ft_putstr_fd("\b\b  \b\b", 1);
 		if (signo != -1)
-			ft_putstr_fd("Quit: 3\n", 1);
+			ft_putstr_fd("^\\Quit: 3\n", 1);
 		else
 			return ;
 	}
